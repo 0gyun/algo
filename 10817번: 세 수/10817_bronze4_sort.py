a434ab1f92f912fd427361ccbfd43ab713de00d7
0gyun <yklee981130@gmail.com>
2025-10-05 13:08:24 +0900

    Directory refactoring

--- 10817번: 세 수/10817_bronze4_sort.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    input = list(map(int, input().split()))
-    answer = sorted(input)[1]
-    print(answer)
--- 5585번: 거스름돈/5585_bronze2_greedy.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    cost = int(input())
-    change = 1000 - cost
-    coins = [500, 100, 50, 10, 5, 1]
-    ans = 0
-    for coin in coins:
-        ans += change // coin
-        change %= coin
-    print(ans)
