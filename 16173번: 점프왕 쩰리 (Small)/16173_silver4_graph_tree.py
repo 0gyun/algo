73d600018e36a95b8feaa62d22eec792cf314295
0gyun <yklee981130@gmail.com>
2025-10-05 12:46:14 +0900

    BOJ-16173

---
+++ 16173번: 점프왕 쩰리 (Small)/16173_silver4_graph_tree.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+import pdb
+def move(N, game_map, y, x):
+    # pdb.set_trace()
+    print(f"Now : ({x}, {y})")
+    if x >= N or y >= N:
+        return 0
+    
+
+    if game_map[y][x] == -1:
+        return 1
+    elif game_map[y][x] == 0:
+        return 0
+    
+    print(f"To : ({x}, {y+game_map[y][x]})")
+    down = move(N, game_map, y+game_map[y][x], x)
+    print(f"To : ({x+game_map[y][x]}, {y})")
+    right = move(N, game_map, y, x+game_map[y][x])
+    return right + down        
+    
+
+N = int(input())
+game_map = []
+for _ in range(N):
+    data = list(map(int, input().split()))
+    row = [d for d in data]
+    game_map.append(row)
+clear = move(N, game_map, 0, 0)
+
+ans = "HaruHaru" if clear > 0 else "Hing"
+print(ans)
